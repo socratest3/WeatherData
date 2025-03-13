b6bc212faf310dba8bdcda7f78597d1903c2364a
socratest3 <socratest3@nycstudents.net>
2025-03-12 20:58:51 -0400

    Update WeatherData.java

--- WeatherData.java
+++ WeatherData.java
@@ -1,1 +1,40 @@
-
+import java.util.ArrayList;
+public class WeatherData
+{
+private ArrayList<Double> temperatures;   
+public WeatherData(ArrayList<Double>temps)
+{
+    temperatures = temps;
+}
+public void cleanData(double lower, double upper)
+{
+    for (int i = 0; i < temperatures.size(); i++)
+    {
+        double temp = temperatures.get(i);
+        if(temp < lower || temp > upper)
+        {
+            temperatures.remove(i);
+            i--;
+        }
+    }
+}
+public String toString()
+{
+    return temperatures.toString();
+}
+public int longestHeatWave(double threshold)
+{
+    int heatWave = 0;
+    int max = 0;
+    for (double t: temperatures)
+    {
+        if(t > threshold)
+        {
+            heatWave++;
+        }
+        if(heatWave > max)max = heatWave; 
+        else heatWave = 0;
+    }
+    return max;
+}
+}
